24e84f570ad2978091dde6171e8a2ca7d29bd4f0
dolly trivedi <dolly.trivedi@seaflux.tech>
2026-01-16 09:17:40 +0530

    feat : update main.dart text by name Dolly

--- lib/main.dart
+++ lib/main.dart
@@ -105,6 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: .center,
           children: [
             const Text('You have pushed the button this many times:'),
+             Text("Hello Flutter First App By Dolly ! "),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
